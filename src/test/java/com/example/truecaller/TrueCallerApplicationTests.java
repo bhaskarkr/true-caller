2748c9fd1cfea4e2843086ffdd0637e4759fc9e8
Bhaskar Kumar <bhaskarkr0811@gmail.com>
2023-11-16 16:59:04 -0700

    create true caller project

---
+++ src/test/java/com/example/truecaller/TrueCallerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.truecaller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrueCallerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
